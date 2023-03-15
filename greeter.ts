4ca925bef7b1ad6b049f27608204817a9b1b6fef
cho1n <cjaewon0217@naver.com>
2023-03-16 00:36:59 +0900

    2023.03.16

--- greeter.ts
+++ greeter.ts
@@ -8,3 +8,16 @@ document.body.textContent = greeter(user); */
 
 const test: string = "Hello Ts World";
 console.log(test);
+
+let str: string = "Hello World!";
+str = "Hello TS";
+console.log(str);
+
+const blog = 'mine-is-back';
+str = `Hello, my ${blog} blog`;
+console.log(str);
+
+let num: number = 6;
+num = 7;
+num = Number('8');
+console.log(num);
